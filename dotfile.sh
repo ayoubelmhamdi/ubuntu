31309c3ef5a53463e11882fb460a91bb4ee910cd
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-23 05:11:26 +0000

    +2

--- dotfile.sh
+++ dotfile.sh
@@ -3,13 +3,11 @@
 export JAVA_HOME='/usr/lib/jvm/java-6-openjdk'
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
 
-git config --global user.name "Ayoub El Mhamdi"
-git config --global user.email "ayoub.elmhamdi@gmail.com"
 
 
-for filename in \
-	$HOME/dotfiles/config-app/.*[^git]  ; do
-    	sudo cp -rf $filename $HOME/${filename##*/}
-    	sudo chown -R $USER:$USER $HOME/${filename##*/}
-done
+#for filename in \
+#	$HOME/dotfiles/config-app/.*[^git]  ; do
+#    	sudo cp -rf $filename $HOME/${filename##*/}
+#    	sudo chown -R $USER:$USER $HOME/${filename##*/}
+#done
 
--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rm -r $HOME/.oh-my-zsh/
-sh  $HOME/debian/zsh-oh.sh
+sh  $HOME/UBUNTU/zsh-oh.sh
 
 git clone \
     https://github.com/zsh-users/zsh-autosuggestions.git \
