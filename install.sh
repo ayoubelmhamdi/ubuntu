06f310ee4a295e4bfb1baff117aaa7bf1e38ca5d
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-23 10:24:05 +0000

    final +2 hude output 3

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 >/dev/null 2>&1
-sh $HOME/UBUNTU/lib.sh     >/dev/null 2>&1 ;echo "lib.sh"
+sh $HOME/UBUNTU/lib.sh     >/dev/null 2>&1 
+echo "lib.sh"
 sh $HOME/UBUNTU/git.sh  
-sh $HOME/UBUNTU/zsh.sh  >/dev/null 2>&1  ;echo "zsh.sh"
-sh $HOME/UBUNTU/dotfile.sh   >/dev/null 2>&1  ;echo "dotfile.shd"
-sh $HOME/UBUNTU/nvim.sh      >/dev/null 2>&1  ;echo "nvim.sh"
+sh $HOME/UBUNTU/zsh.sh  >/dev/null 2>&1  
+echo "zsh.sh"
+sh $HOME/UBUNTU/dotfile.sh   >/dev/null 2>&1  
+echo "dotfile.shd"
+sh $HOME/UBUNTU/nvim.sh      >/dev/null 2>&1  
+echo "nvim.sh"
 
