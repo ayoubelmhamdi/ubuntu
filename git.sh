00f73f6b0bdbce19bdffb1a906019217db7ce896
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-23 05:41:43 +0000

    gut +3

--- git.sh
+++ git.sh
@@ -22,7 +22,7 @@ echo "\n\n\t" 										\
      "ssh-key :\n\t" 								\
 
 cat  $HOME/.ssh/id_ed25519.pub 						\
-  >> $HOME/debian-ssh-github.txt					\
+  >> $HOME/UBUNTO-ssh-github.txt					\
 
 echo "\n\n\t" 										\
      "add this line to :\n\n" 						\
@@ -30,10 +30,10 @@ echo "\n\n\t" 										\
      "for git private me@scripts frome github\n"	\
 	 "git clone" 									\
      "git@github.com:ayoubelmhamdi/scripts.git\n"	\
-	>> $HOME/debian-ssh-github.txt
+	>> $HOME/UBUNTO-ssh-github.txt
 
 
-cat  $HOME/debian-ssh-github.txt
+cat  $HOME/UBUNTO-ssh-github.txt
 
 #++++++++++++++++++++++++++++++++++++++++
 
@@ -42,13 +42,13 @@ git config --global user.email "ayoub.elmhamdi@gmail.com"
 
 #++++++++++++++++++++++++++++++++++++++++
 
-cd $HOME/debian
+cd $HOME/UBUNTO
 git remote set-url origin \
-    git@github.com:ayoubelmhamdi/debian.git
+    git@github.com:ayoubelmhamdi/ubuntu.git
     
 cd $HOME/dotfiles
 git remote set-url origin \
-    git@github.com:ayoubelmhamdi/dotfiles-ayoub.git
+    git@github.com:ayoubelmhamdi/dotfiles.git
 
 #because is private
 #cd $HOME/scripts
