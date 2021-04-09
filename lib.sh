fff9cfbd5ec49dbfd075b4d2551b82fe7ebb0a7d
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-04-09 09:48:55 +0100

    lib update

--- lib.sh
+++ lib.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-#~/.fzf/install
+mkdir -p $HOME/.config/coc/extensions 
 
-
-#sudo  chown -R $USER:$(id -gn $USER) /home/deb36/.config
-#npm config set registry http://registry.npmjs.org
-#echo "y" | sudo npm install -g @angular/cli
-#sh $HOME/debian/nodejs-12.sh
-
-mkdir -p $HOME/.config/coc/extensions/ 
-mkdir -p $HOME/.config/nvim/
-
+# reset version coc-extenstion 
 path-coc-ex1="$HOME/.config/coc/extensions"
-path-coc-ex3="$HOME/.config/nvim/package.json"
+path-coc-ex3="$HOME/.config/nvim"
 
-[ ! -f path-coc-ex1 ] && echo '{"dependencies":{}}'> package.json
-[ ! -f path-coc-ex2 ] && echo '{"dependencies":{}}'> package.json
+[ ! -f path-coc-ex1 ] && echo '{"dependencies":{}}'> path-coc-ex1/package.json
+[ ! -f path-coc-ex2 ] && echo "{}"                 > path-coc-ex3/package.json
 
 
 
@@ -49,25 +40,19 @@ sudo apt install     -y   \
 
 
 curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt install -y \
-      	aptitude        \
-        nodejs          
+sudo apt install -y      \
+         aptitude        \
+         nodejs          
+
 sudo aptitude install -y npm 
 
-# reset version coc-extenstion 
 # unlock path of neovim provider
-export PATH=$PATH:$HOME/.local/bin/
-echo "{}" > $HOME/.config/coc/extensions/package.json
-
-sudo chown $USER /usr/local/lib/
-sudo chown $USER $HOME/.cache/pip/
-sudo chown $USER $HOME/.cache/pip/http/
-sudo chown $USER $HOME/.local/lib/python3*
-sudo chown $USER $HOME/.local/lib/python2*
-
 # used for fix provider3
+
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --force-reinstall
+rm get-pip.py
+
 
 pip3 --default-timeout=1000 install \
         pynvim            \
@@ -79,35 +64,36 @@ pip2 install --user --upgrade  --default-timeout=10000  pynvim
 python3 -m pip install --upgrade pynvim
 python2 -m pip install --user --upgrade pynvim
 
-npm i npm                    \
-      bash-language-server   \
-      eovim
+npm i npm 
+npm i                      \
+    bash-language-server   \
+    eovim
 
 #sudo npm install         \
-#	 coc-explorer		  \
-#	 coc-snippets		  \
-#	 coc-json			  \
-#	 coc-actions		  \
-#  	 coc-java			  \
-#  	 coc-lists			  \
-#  	 coc-emmet			  \
-#  	 coc-tasks			  \
-#  	 coc-pairs			  \
-#  	 coc-tsserver		  \
-#  	 coc-floaterm		  \
-#  	 coc-html			  \
-#  	 coc-css		 	  \
-#  	 coc-cssmodules		  \
-#  	 coc-stylelintplus	  \
-#  	 coc-emoji			  \
-#  	 coc-yaml			  \
-#  	 coc-pyright		  \
-#  	 coc-svg			  \
-#  	 coc-prettier		  \
-#  	 coc-vimlsp			  \
-#  	 coc-xml			  \
-#  	 coc-yank			  \
-#  	 coc-sh				  \
+#    coc-explorer        \
+#    coc-snippets        \
+#    coc-json           \
+#    coc-actions        \
+#      coc-java           \
+#      coc-lists           \
+#      coc-emmet           \
+#      coc-tasks           \
+#      coc-pairs           \
+#      coc-tsserver        \
+#      coc-floaterm        \
+#      coc-html           \
+#      coc-css            \
+#      coc-cssmodules        \
+#      coc-stylelintplus     \
+#      coc-emoji           \
+#      coc-yaml           \
+#      coc-pyright        \
+#      coc-svg           \
+#      coc-prettier        \
+#      coc-vimlsp           \
+#      coc-xml           \
+#      coc-yank           \
+#      coc-sh              \
 #     --global-style    \
 #     --ignore-scripts  \
 #     --no-bin-links    \
@@ -117,12 +103,12 @@ npm i npm                    \
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-git clone 					 \
-    https://github.com/ayoubelmhamdi/nvim.git	 \
+git clone                                          \
+    https://github.com/ayoubelmhamdi/nvim.git      \
     $HOME/.config/nvim 
 
 git clone \
-    https://github.com/neovim/neovim.git	\
+    https://github.com/neovim/neovim.git           \
     $HOME/neovim 
 
 git clone --bare \
