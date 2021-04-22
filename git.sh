5a363d2a503e36ca87cf81755c8ae4e7629cd8c2
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-04-22 15:49:22 +0100

    git fix

--- git.sh
+++ git.sh
@@ -21,13 +21,13 @@ echo -e "\n\n\t" 										\
      "ssh-key :\n\t" 								\
 
 cat  $HOME/.ssh/id_ed25519.pub 						\
-  >> $HOME/TERMUX-ssh-github.txt					\
+>>   $HOME/TERMUX-ssh-github.txt					\
 
-echo -e "\n\n\t" 										\
+echo "\n\n\t" 										\
      "add this line to :\n\n" 						\
      "https://github.com/settings/ssh/new \n\n\t"	\
      "for git private me@scripts frome github\n"	\
-	 "git clone" 									\
+	   "git clone" 									\
      "git@github.com:ayoubelmhamdi/scripts.git\n"	\
 	>> $HOME/TERMUX-ssh-github.txt
 
